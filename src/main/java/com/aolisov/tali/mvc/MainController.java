ff8529fdc5ce838a3b61f1c9b454e117743f619a
Alex <sanchokus@gmail.com>
2016-04-16 20:39:11 +0300

    Better output for response command

--- src/main/java/com/aolisov/tali/mvc/MainController.java
+++ src/main/java/com/aolisov/tali/mvc/MainController.java
@@ -2,15 +2,15 @@ package com.aolisov.tali.mvc;
 
 import com.aolisov.tali.data.properties.AppProperties;
 import com.aolisov.tali.data.properties.PropertiesHolder;
-import com.aolisov.tali.data.store.ResponseEntry;
 import com.aolisov.tali.data.store.ResponseStore;
+import com.aolisov.tali.misc.AppJsonMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
-import java.util.Map;
 
 /**
  * Created by Alex on 4/14/2016.
@@ -40,7 +40,7 @@ public class MainController {
 
     @RequestMapping(value = "/response")
     @ResponseBody
-    public Map<String,ResponseEntry> getResponse() {
-        return responseStore.get();
+    public String getResponse() throws JsonProcessingException {
+        return new AppJsonMapper().writeValueAsString(responseStore.get());
     }
 }
